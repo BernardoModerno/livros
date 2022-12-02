c7438e8d3d7620da6634ba0a055e36d47513636a
BernardoModerno <bernardo.moderno@gmail.com>
2022-12-02 11:31:48 -0300

    componente NavBar

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Limpando aplicação</h1>
-    </div>
-  );
+    <Navbar />
+  )
 }
 
 export default App;
